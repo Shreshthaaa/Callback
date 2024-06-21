780b8ac5f230dab49e7966e2a1fc1d6d726c6430
TicTacToe_sst <paliwalshreshtha@gmail.com>
2024-06-21 11:17:17 +0530

    useCallback

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+import './App.css';
+import React, {useCallback, useState} from 'react';
+
+function App() {
+  const [input, setInput] = useState("");
+  const [count, setCount] = useState(0);
+  const incrementCount = useCallback(() => setCount(count+1), [count]);
+
+  return (
+    <div className="App">
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={incrementCount}>Increment counter</button>
+      <h3>Input text: {input}</h3>
+      <h3>Count: {count}</h3>
+      <hr />
+      <ChildComponent count={count} onClick={incrementCount}/>
+    </div>
+  );
+}
+
+const ChildComponent = React.memo(function({count, onClick }) {
+  console.log("child component is rendering");
+  return (
+    <div>
+      <h2>This is a child component.</h2>
+      <button onClick={onClick}>Increment</button>
+      <h4>Count: {count}</h4>
+    </div>
+  );
+})
+
+export default App;
